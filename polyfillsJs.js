4630b86bb480b757e7c2800c12248d5a2666fd7c
Maxim Chizhevskiy <max15.91@mail.ru>
2024-09-08 23:05:15 +0500

    Update polyfillsJs.js

--- polyfillsJs.js
+++ polyfillsJs.js
@@ -1,8 +1,10 @@
-//map-------------------------------------------------------
 const numbers = [1, 2, 3, 4, 5]
-const multiplyBy10 = numbers.map(el => el*10)
+const fruits = ['apple', 'banana', 'cherry', 'pear', 'orange', 'strawberry']
 
-const multiplyBy10Func = (numbersArr, callback) => {
+//map-------------------------------------------------------
+const resultMap = numbers.map(el => el * 10)
+
+const mapFunc = (numbersArr, callback) => {
     const newArray = []
     for (let i = 0; i < numbersArr.length; i++) {
         newArray.push(callback(numbersArr[i]))
@@ -10,13 +12,12 @@ const multiplyBy10Func = (numbersArr, callback) => {
     return newArray
 }
 
-const resultMultiply = multiplyBy10Func(numbers, (num) => num * 10)
-console.log(resultMultiply)
+const resultMapFunc = mapFunc(numbers, (num) => num * 10)
+console.log(resultMapFunc)
 
 //slice--------------------------------------------------------
-const fruits = ['apple', 'banana', 'cherry', 'pear', 'orange', 'strawberry']
 const resultSlice = fruits.slice(1, 4)
-
+console.log("slice ", resultSlice)
 const sliceFunc = (start, end, fruitsArr) => {
     const newArray = []
     for (let i = start; i < end; i++) {
@@ -26,6 +27,68 @@ const sliceFunc = (start, end, fruitsArr) => {
     return newArray
 }
 
-const resultSliceFunc = sliceFunc(1, 3, fruits)
-console.log(resultSliceFunc)
+const resultSliceFunc = sliceFunc(1, 4, fruits)
 
+//splice---------------------------------------------------------
+const resultSplice = fruits.splice(1, 2)
+
+const spliceFunc = (start, count, arr, replaceArr) => {
+    for (let i = start; i < start + count; i++) {
+        arr[i] = replaceArr[i - start]
+    }
+    return arr
+}
+
+const resultSpliceFunc = spliceFunc(1, 2, fruits, ['NEWBanana', 'NEWCherry'])
+console.log(resultSpliceFunc)
+
+//reduce
+const resultReduce = numbers.reduce((acc, currentValue) => {
+    return acc += currentValue
+}, 0)
+
+console.log(resultReduce)
+
+const reduceFunc = (numbersArr) => {
+    let acc = 0
+    for (let i = 0; i < numbersArr.length; i++) {
+        acc += numbersArr[i]
+    }
+    return acc
+}
+
+const resultReduceFunc = reduceFunc(numbers)
+console.log(resultReduceFunc)
+
+//reduce extra tasks
+const fruit1 = ['apple', 'banana', 'cherry', 'apple', 'banana', 'pear', 'orange', 'banana', 'strawberry']
+
+const countOfFruits = fruit1.reduce((acc, currentValue) => {
+    if (acc[currentValue]) {
+        acc[currentValue] += 1
+    } else {
+        acc[currentValue] = 1
+    }
+    return acc
+}, {})
+
+console.log(countOfFruits)
+
+const veg = [
+    {name: 'apple', prices: 25, kg: 10},
+    {name: 'orange', prices: 125, kg: 100},
+    {name: 'tomato', prices: 225, kg: 1000},
+    {name: 'tomato', prices: 225, kg: 1000},
+]
+
+const countOfVegetables = veg.reduce((acc, currentValue) => {
+    if (acc[currentValue.name]) {
+        acc[currentValue.name] += currentValue.prices * currentValue.kg
+    } else {
+        acc[currentValue.name] = currentValue.prices * currentValue.kg
+    }
+
+    return acc
+}, {})
+
+console.log(countOfVegetables)
